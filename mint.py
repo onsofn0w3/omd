4c8e6a4e37cd5b75a13fed739f80339ca3f87778
dortheclosed <120745103+dortheclosed@users.noreply.github.com>
2023-04-28 13:11:05 +0300

    Update mint.py

--- mint.py
+++ mint.py
@@ -1,17 +1,17 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
-import undetected_chromedriver as uc
+#import undetected_chromedriver as uc
 
 def run_session():
-    chrome_options = uc.ChromeOptions()
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.headless = True 
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument('--no-first-run --no-service-autorun --password-store=basic')
     chrome_options.add_argument(f'--disable-gpu')
     
     chrome_options.add_argument(f'--disable-dev-shm-usage')
-    driver = uc.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(options=chrome_options)
     try:
         driver.get("https://mrscan.onrender.com")
         print(f" connected to page")
